51a13c84e6a3f4d11bd87c6d19ddf1061b68660a
hafizjoundy <hafizjoundys@gmail.com>
2017-06-12 05:12:30 +0700

    update

--- objectsinau/index.php
+++ objectsinau/index.php
@@ -1,14 +1,43 @@
 <?php
-class laptop
-{
-  public static $harga;
-  public static function belicom()
-  {
-    return "Beli komputer dengan harga".self::$harga;
-  }
+// buat abstract class
+abstract class komputer{
+   // buat abstract method
+   abstract public function booting_os();
 }
 
-laptop::$harga = 1000000;
-echo laptop::belicom();
+class laptop extends komputer{
+   public function booting_os(){
+     return "Proses Booting Sistem Operasi Laptop";
+   }
+}
 
+class pc extends komputer{
+   public function booting_os(){
+     return "Proses Booting Sistem Operasi PC";
+   }
+}
+
+class chromebook extends komputer{
+public function booting_os(){
+     return "Proses Booting Sistem Operasi Chromebook";
+   }
+}
+
+
+// buat objek dari class diatas
+$laptop_baru = new laptop();
+$pc_baru = new pc();
+$chromebook_baru = new chromebook();
+
+// buat fungsi untuk memproses objek
+function booting_os_komputer($objek_komputer){
+   return $objek_komputer->booting_os();
+}
+
+// jalankan fungsi
+echo booting_os_komputer($laptop_baru);
+echo "<br />";
+echo booting_os_komputer($pc_baru);
+echo "<br />";
+echo booting_os_komputer($chromebook_baru);
  ?>
